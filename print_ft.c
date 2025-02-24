a78f180bc80883f79517d23ff7237f09a62745e9
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2025-02-24 21:48:22 +0000

    implementacion del convertidor de formatos

--- print_ft.c
+++ print_ft.c
@@ -1,59 +1,114 @@
 #include <unistd.h>
-
+#include <stdarg.h>
+// Función para imprimir un solo character
 int ft_putchar(char c)
 {
     return write(1, &c, 1);
 }
 
+// Función para imprimir una string
 int ft_putstr(char *str)
 {
-    int i;
-    i = 0;
-
-    if(str == NULL)
+    int count = 0;
+    if (!str)
+        str = "(null)";
+    while (*str)
     {
-        write(1, "(null)", 6);
-        return(6);
+        count += ft_putchar(*str);
+        str++;
     }
-    while(str[i])
-        write(1, &str[i++], 1);
-    return(i);
+    return count;
 }
 
-int ft_conversion(va_list vl, const char format)
+// Función para imprimir un número en base 10
+int ft_putnbr(int n)
 {
-    int char_printed;
-
-    char_printed = 0;
-
-    if(format == 'c')
+    int count = 0;
+    if (n == -2147483648)
+        return ft_putstr("-2147483648");
+    if (n < 0)
     {
-        char_printed += ft_putchar(va_arg(vl, int));
+        count += ft_putchar('-');
+        n = -n;
     }
-    else if (format == 's')
-    {
-        char_printed += ft_putstr(va_arg(vl, char*));
-    }
-    else if(format == 'p')
-    {
-
-    }
-    else if(format == 'd')
-    {
-
-    }
-    else if(format == 'u')
-    {
-
-    }
-    else if(format == 'x')
-    {
-
-    }
-    else if(format)
+    if (n > 9)
+        count += ft_putnbr(n / 10);
+    count += ft_putchar(n % 10 + '0');
+    return count;
 }
 
-int ft_printf(char const *, ...)
-{
+// int ft_conversion(va_list vl, const char format)
+// {
+//     int char_printed;
 
+//     char_printed = 0;
+
+//     if(format == 'c')
+//     {
+//         char_printed += ft_putchar(va_arg(vl, int));
+//     }
+//     else if (format == 's')
+//     {
+//         char_printed += ft_putstr(va_arg(vl, char*));
+//     }
+//     else if(format == 'p')
+//     {
+
+//     }
+//     else if(format == 'd')
+//     {
+
+//     }
+//     else if(format == 'u')
+//     {
+
+//     }
+//     else if(format == 'x')
+//     {
+
+//     }
+//     else if(format)
+// }
+
+int ft_printf(const char *format, ...)
+{
+    va_list args;
+    int count;
+
+    count = 0;
+
+    va_start(args, format); //inicializa args permitiendo extraer los valores pasados después de format
+    while(*format)
+    {
+        if(*format == '%')
+        {
+            format++;
+            if(*format == 'c')
+            {
+                count+= ft_putchar(va_arg(args, int)); //extrae los argumentos
+            }
+            else if (*format == 's')
+            {
+                count += ft_putstr(va_arg(args, char *));
+            }
+            else if (*format == 'i' || *format == 'd')
+            {
+                count += ft_putnbr(va_arg(args, int)); 
+                //se agregan más conversiones
+            }
+        }
+        else
+        {
+            count += ft_putchar(*format);
+        }
+        format++;
+    }
+    va_end(args); //finaliza el uso de arg variádicos; limpia la lista de argumentos
+    return count; //devuelve el numero total de caracteres impresos
+}
+
+int main()
+{
+    ft_printf("Hola %s tengo %i", "Juan", 22);
+    return (0);
 }
